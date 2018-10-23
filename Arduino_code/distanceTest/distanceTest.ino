dea50112da7c5efa26d872301175112c3e6d3411
dberny <31549375+dberny@users.noreply.github.com>
2018-10-23 12:12:06 -0400

    added code to rotate motor through 1 and 2 revolutions

---
+++ Arduino_code/distanceTest/distanceTest.ino
@@ -0,0 +1,43 @@
+/*     Simple Stepper Motor Control Exaple Code
+ *      
+ *  by Dejan Nedelkovski, www.HowToMechatronics.com
+ *  
+ */
+
+// 29.751 MM PER REVOLUTION
+// 0.018594 MM PER STEP
+ 
+// defines pins numbers
+const int stepPin = 54; 
+const int dirPin = 55;
+const int enablePin = 38; 
+ 
+void setup() {
+  // Sets the two pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  pinMode(enablePin,OUTPUT);
+
+  digitalWrite(enablePin,LOW);
+}
+void loop() {
+  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < 3200; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(500); 
+  }
+  delay(1000); // One second delay
+  
+  digitalWrite(dirPin,LOW); //Changes the rotations direction
+  // Makes 400 pulses for making two full cycle rotation
+  for(int x = 0; x < 6400; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
+}
