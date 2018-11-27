0437ae6b10b7312124e21c1514708c7466ead155
Diego Berny <dberny@olin.edu>
2018-11-26 22:25:17 -0500

    added new shape cutting ability

--- gcode_conversion/millToWireGcodeConverter.py
+++ gcode_conversion/millToWireGcodeConverter.py
@@ -9,7 +9,7 @@ with open('triangleGcode.txt', 'r+') as file:
         newline = ''
         for word in words:
             if (word[0] == 'X'):
-                newline = newline + ' ' + word + ' U' + word[1:]
+                newline = newline + ' ' + word + ' U-' + word[1:]
             elif (word[0] == 'Y'):
                 newline = newline + ' ' + word + ' Z' + word[1:]
             elif (word[0] == 'Z'):
@@ -18,16 +18,19 @@ with open('triangleGcode.txt', 'r+') as file:
             elif (word[0] == 'G'):
                 newline += ' ' + word
             elif (word[0] == 'F'):
-                newline += ' ' + word
+                newline += ' ' + word[0] + '350'
 
         if len(newline) > 0:
             newline += '\n'
         print(newline)
 
-        with open('triangleTest.txt', 'a') as newfile:  # add modified line to new file
-            newfile.write(newline)
+        num = newline.split()
+        if len(num) > 1:
+            with open('triangleTest.txt', 'a') as newfile: # add modified line to new file
+                newfile.write(newline)
 
-
+    with open('triangleTest.txt', 'a') as newfile:  # add modified line to new file
+                    newfile.write('G01 X0 U0 Y0 Z0')
 #write file again
 # with open('newfile.txt', 'w') as file:
 #   file.write(lines)
